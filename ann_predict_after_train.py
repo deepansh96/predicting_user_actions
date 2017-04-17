f75498827fd4c875de02d6fe50f539d993bceede
deepansh96 <deepansh.mathur96@gmail.com>
2017-04-17 21:53:34 +0530

    First commit in this repo

---
+++ ann_predict_after_train.py
@@ -0,0 +1,33 @@
+import numpy as np
+from process import get_data
+import ann_train
+
+
+X, Y = get_data()
+
+# randomly initialize weights
+M = 5
+D = X.shape[1]
+K = len(set(Y))
+W1 = ann_train.W1
+b1 = ann_train.b1
+W2 = ann_train.W2
+b2 = ann_train.b2
+
+# make predictions
+def softmax(a):
+    expA = np.exp(a)
+    return expA / expA.sum(axis=1, keepdims=True)
+
+def forward(X, W1, b1, W2, b2):
+    Z = np.tanh(X.dot(W1) + b1)
+    return softmax(Z.dot(W2) + b2)
+
+P_Y_given_X = forward(X, W1, b1, W2, b2)
+predictions = np.argmax(P_Y_given_X, axis=1)
+
+# calculate the accuracy
+def classification_rate(Y, P):
+    return np.mean(Y == P)
+
+print "Score:", classification_rate(Y, predictions)
